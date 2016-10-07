cdf877c6ea70029003d45de24c7e99d0dd21d020
Andrew <andrew@EwertCS00.local>
2016-10-07 15:51:40 +0100

    Updated language specification Developed main.c for mmltomidi

--- mmltomidi/main.c
+++ mmltomidi/main.c
@@ -1,8 +1,32 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool callValid(int argc, char *argv[]) {
+	if ((argc < 2) || (argc > 4)) {
+		return false;
+	}
+	
+	if (strcmp(argv[1], "-o") == 0) {
+		//Test in & out file existence and access
+		
+	} else {
+		//Test in & out file existence and access	
+		
+	}
+	
+	return true;
+}
 
 int main(int argc, char *argv[]) {
-	printf("Hello world!\n");
+	if (!callValid(argc, argv)) {
+		fprintf(stderr, "Usage: mmltomidi [-o output_path] [file]\n");
+		
+		return 0;
+	}
+	
+	
 
 	return 0;
 }
