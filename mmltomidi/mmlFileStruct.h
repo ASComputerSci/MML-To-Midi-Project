d99a1bfa3323b9c01b8c85290f352b8ae0a330f0
Andrew <andrew.small@ox1.co.uk>
2017-01-08 16:58:33 +0000

    Developed lex.l Modified mmlFile struct definition

--- mmltomidi/mmlFileStruct.h
+++ mmltomidi/mmlFileStruct.h
@@ -3,16 +3,18 @@
 
 //Necessary for only one copy of the struct definition
 
+struct note {
+	char command; //Letter
+	char accidental; //-1 for flat, 1 for sharp
+	char modifier; //Number after
+};
+
 struct mmlFileStruct {
 	char name[256]; //Null terminated
 	unsigned char tempo;
 	unsigned char instrument;
+	
+	struct note *notes;
 }; 
-
-struct note {
-	char command;
-	char accidental; //-1 for flat, 1 for sharp
-	char modifier;
-};
 
 #endif
